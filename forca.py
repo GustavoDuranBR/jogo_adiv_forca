dc1243cae7d181a7db30c7b1d5ce0d23618ebfcc
Gustavo Duran <gustavoduran22@icloud.com>
2021-06-03 23:11:19 -0300

    Melhoria do código criando funções.

--- forca.py
+++ forca.py
@@ -1,13 +1,38 @@
 from funcoes_do_jogo import *
-from random import randint
-
 
 def forca():
-
-
     titulo_frc()
+    palavra_secreta = carrega_palavra_secreta()
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
+    print(letras_acertadas)
+    
+    enforcou = False
+    acertou = False
+    erros = 0
 
-    print('Vamos jogar?\nElaborar frase do jogo FORCA.')
+
+    print('Vamos jogar?\n')
+
+    while not acertou and not enforcou:
+        chute = pede_chute()
+
+        if chute in palavra_secreta:
+            marca_chute_correto(chute, letras_acertadas, palavra_secreta)
+        else:
+            erros += 1
+            desenha_forca(erros)
+            print(desenha_forca)
+
+        enforcou = erros == 7
+        acertou = "_" not in letras_acertadas
+
+        print(letras_acertadas)
+
+    if acertou:
+        imprime_vencedor()
+    else:
+        imprime_perdedor(palavra_secreta)
+
     fim()
 
 
